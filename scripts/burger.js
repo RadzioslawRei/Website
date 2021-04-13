5c2fc60ac94c50e35c463fb25067f488231c4add
unknown <radoslaw.reichel@onet.pl>
2021-04-13 21:39:21 +0200

    Main and Navbar

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,12 +1,19 @@
 var button_burger = document.querySelector('.burger');
+var nav = document.querySelector('.nav');
 var menuOpen = false;
 function menuButton() {
     if(!menuOpen){
         button_burger.classList.add('open');
+        nav.style.opacity = '1';
+        nav.style.transform = 'translate(-100%,-15px)';
+        nav.style.transition = 'all .5s ease-in-out';
         menuOpen = true;
     }
     else{
         button_burger.classList.remove('open');
+        nav.style.opacity = '0';
+        nav.style.transform = 'translate(-90%, -15px)';
+        nav.style.transition = 'all .5s ease-in-out';
         menuOpen = false;
     }
 };
