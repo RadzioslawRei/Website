71367a82d0fe6d59c46e074640071b0c78242696
unknown <radoslaw.reichel@onet.pl>
2021-04-14 20:29:52 +0200

    main and withdrawal navbar

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,19 +1,16 @@
 var button_burger = document.querySelector('.burger');
 var nav = document.querySelector('.nav');
+nav
+var navitem1 = document.querySelector('.nav-item1');
+var navitem2 = document.querySelector('.nav-item2');
 var menuOpen = false;
 function menuButton() {
     if(!menuOpen){
         button_burger.classList.add('open');
-        nav.style.opacity = '1';
-        nav.style.transform = 'translate(-100%,-15px)';
-        nav.style.transition = 'all .5s ease-in-out';
         menuOpen = true;
     }
     else{
         button_burger.classList.remove('open');
-        nav.style.opacity = '0';
-        nav.style.transform = 'translate(-90%, -15px)';
-        nav.style.transition = 'all .5s ease-in-out';
         menuOpen = false;
     }
 };
